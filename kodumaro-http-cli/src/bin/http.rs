6fe73b755dd548b910ef2c4fcc8da020f8693e08
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2024-10-09 17:56:51 -0300

    let clap deal with errors

--- kodumaro-http-cli/src/bin/http.rs
+++ kodumaro-http-cli/src/bin/http.rs
@@ -1,17 +1,10 @@
-use clap::{error::ErrorKind, Parser};
+use clap::Parser;
 use eyre::Result;
 use kodumaro_http_cli::Cli;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut cl_parameters = match Cli::try_parse() {
-        Ok(args) => args,
-        Err(err) if [ErrorKind::DisplayHelp, ErrorKind::DisplayVersion].contains(&err.kind()) => {
-            eprintln!("{}", err);
-            return Ok(());
-        }
-        Err(err) => return Err(err.into()),
-    };
+    let mut cl_parameters = Cli::parse();
     cl_parameters.initialize()?;
     kodumaro_http_cli::perform(cl_parameters).await?;
     Ok(())
